1b5bca80977be014ae4c6584b5ee07d4a6fb99b6
Simple <pix.59700@gmail.com>
2022-10-07 13:48:48 +0200

    Add files for player ✅
    Add Fps Cam ("PlayerCam")
    Add Mouvement player ("PlayerMouvement")

---
+++ Assets/_Core/Script/Player/PlayerMouvement.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+    [Header("Mouvement"), Space]
+    [SerializeField] float moveSpeed;
+
+    [SerializeField] float groundDrag;
+
+    [SerializeField] float jumpForce;
+    [SerializeField] float jumpCooldown;
+    [SerializeField] float aireMultiplier;
+    bool readyToJump;
+
+    [Header("Keybinds"), Space]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check"), Space]
+    [SerializeField] float playerHeight;
+    [SerializeField] LayerMask whatIsGround;
+    bool grounded;
+
+    [SerializeField] Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+        // add layer "whatIsGround" in a plane.
+    }
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * aireMultiplier, ForceMode.Force);
+        }
+    }
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, limitedVel.y, limitedVel.z);
+        }
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
