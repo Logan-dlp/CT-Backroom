49ec72998251199360425e8f07573975081c7bfb
Simple <pix.59700@gmail.com>
2022-10-07 15:46:34 +0200

    Add files 📝
    Add Die of the player

---
+++ Assets/_Core/Script/Player/Die.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        if (rb != null) return;
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnTriggerEnter(Collider _colider)
+    {
+        MonsterDetection _detect = _colider.gameObject.GetComponent<MonsterDetection>();
+        if (_detect != null) return;
+        transform.position = new Vector3(0, 0, 0);
+    }
+}
